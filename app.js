62b26ec846a3c622b1369e478bcaf4b72eb23cc7
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2020-02-17 13:29:52 +0300

    Занятие 3 - типы данных

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// let a = 2;
+// let a = 'ppppp';
+// let a = true;
+// let a = null;
+let a = undefined;
+console.log(a);
+console.log(typeof a);
+
+
 // alert('ddddfsdfsdfsdfd');
 
 // test
@@ -8,12 +17,12 @@ test
 test
 */
 
-let a = 2;
-let b = 4;
-let c = 10;
-
-console.log("a =", a, ",b =", b);
-b = c;
-
-let cona = console.log(a + b);
-console.log(cona);
+// let a = 2;
+// let b = 4;
+// let c = 10;
+//
+// console.log("a =", a, ",b =", b);
+// b = c;
+//
+// let cona = console.log(a + b);
+// console.log(cona);
