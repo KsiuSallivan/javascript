202aaa47602c24c754136dbe7f47fce15422db79
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2020-02-17 14:28:26 +0300

    Занятие 5 - знакомство с объектами

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-// let a = 2;
+let car = {
+  color: 'green',
+  maxSpeed: 250,
+  audio: {
+    brand: 'Sony',
+    speakers: 12
+  }
+};
+
+// let carProp = 'color';
+//
+// car[carProp] = 'red';
+car.color = 'red';
+// console.log(typeof car);
+console.log(car.audio.brand);
+
+
+
+
 // let a = 'ppppp';
 // let a = true;
 // let a = null;
@@ -7,11 +25,11 @@
 // let b = 10;
 // let a = `Он сказал ${b} раз Привет`;
 
-let a = Symbol();
-let b = Symbol();
-
-console.log(a === b);
-console.log(typeof a);
+// let a = Symbol();
+// let b = Symbol();
+//
+// console.log(a === b);
+// console.log(typeof a);
 
 
 // alert('ddddfsdfsdfsdfd');
