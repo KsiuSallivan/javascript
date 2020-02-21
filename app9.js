6efdd8417c11634360e555af49588b783b900321
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2020-02-21 12:17:34 +0300

    lesson 9 - методы и массивы

---
+++ app9.js
@@ -0,0 +1,27 @@
+// Занятие 9
+//
+// function getMessage() {
+//   console.log('Я функция');
+// }
+
+// let car = {
+//   color: 'red',
+//   open(message) {
+//     console.log(message);
+//   }
+// }
+//
+// car.open('открыто');
+
+
+// let array = [1, 3, 10, 69] // негомогенный массив
+//
+// console.log(array[2]);
+
+let car = {
+  color: 'red'
+}
+
+let array = [10, 'текст', 'другой текст', {color: 'red'}]
+
+console.log(array[1] + ' ' + array[2]);
