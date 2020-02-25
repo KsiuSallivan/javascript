0d2f66e9410e7073a2b131e8fa8a524f91b4fd44
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2020-02-25 11:02:50 +0300

    lesson 10 - циклы

---
+++ app10.js
@@ -0,0 +1,30 @@
+// let i = 1;
+// // i++;
+// // ++i;
+// // i--;
+// --i;
+// console.log(i);
+
+
+let drivers = ['Ivan', 'Sergey', 'Egor'];
+
+// console.log(drivers[0]);
+// console.log(drivers[1]);
+
+console.log(drivers.length);
+
+// for (let i = 0; i < drivers.length; i++) {
+//   console.log(drivers[i]);
+// }
+
+// for (let i = drivers.length - 1; i >= 0; i--) {
+//   console.log(drivers[i]);
+// }
+
+
+// drivers.reverse();
+// console.log(drivers);
+
+
+let newArray = drivers.reverse();
+console.log(drivers === newArray);
