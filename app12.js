22cb80db2404f60bbcb362c2135b5d2b63a7582b
Kseniya Kurashova <k.kurashova@rtk-dc.ru>
2020-02-27 10:53:50 +0300

    lesson 12 - DOM

---
+++ app12.js
@@ -0,0 +1,12 @@
+
+// console.log(document.getElementById('header'));
+
+// console.log(typeof document.getElementById('header'));
+
+// document.getElementById('header').innerText = 'Hello';
+
+// let findElement = document.querySelector('div.hihi');
+// console.log(findElement);
+
+let findElement = document.querySelector('[value="30"]');
+console.log(findElement);
